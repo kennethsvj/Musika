851769c82aaab1aa23fdd1e503b2b0d29fbb3348
kennethsvj <kenneth.svj@gmail.com>
2026-05-05 09:21:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@
 
 window.MUSIKA_CONFIG = {
   SUPABASE_URL: 'https://emhuwjlkfwwapyhmnirq.supabase.co',
-  SUPABASE_ANON_KEY: 'PASTE_YOUR_ANON_KEY_HERE',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmYnp1enV6cHlobnZqcmhham5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyNDQxODgsImV4cCI6MjA5MTgyMDE4OH0.ERpe5nHFfd9r6lbEZ9bkQzkkHvTj-k-u4G9N1ZI3N2w',
 };
